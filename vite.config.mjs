b1a77922aedcd178b4502e2dd2a897203a5ef463
Menardi <Menardi@users.noreply.github.com>
2024-05-13 16:55:23 +0100

    Set relative base in Vite config to fix deployment issue

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // allows the site to be deployed on any path, not just the root
   plugins: [
     preact(),
   ],
