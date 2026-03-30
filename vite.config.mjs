14efecd3d17aa62194e3d588249388c159ecb108
Menardi <Menardi@users.noreply.github.com>
2026-03-30 11:09:44 +0100

    Update dev server to always return 404s for missing files

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './', // allows the site to be deployed on any path, not just the root
+  appType: 'mpa', // ensures the dev server returns 404s for missing files
   plugins: [
     preact(),
   ],
